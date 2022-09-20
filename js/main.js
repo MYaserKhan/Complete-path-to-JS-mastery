a1022e71d71fa0edd826c489de9add8e0d26402a
Yaser <khanyaser007@gmail.com>
2022-09-20 13:13:57 +0500

    Add JS array of objects in main.js with images

--- js/main.js
+++ js/main.js
@@ -5,3 +5,56 @@ hamburger.addEventListener(('click'), () => {
   hamburger.classList.toggle('active');
   navbar.classList.toggle('action');
 });
+
+// Array of project objects
+const seeMore = document.querySelector('.seemore');
+const seeInstructor = document.querySelector('.instructor-section');
+const instructorSection = document.querySelector('.instructor-container');
+
+seeMore.addEventListener(('click'), () => {
+  seeInstructor.classList.toggle('setoff');
+});
+
+const instructorContainer = [
+  {
+    name: 'Joseph Gonzalez',
+    title: 'Main Instructor',
+    description: 'Joseph has been leading open-source projects at GitHub. Mostly related to JavaScript',
+    Image: 'img/joseph.jpg',
+  },
+  {
+    name: 'Jurica Koletic',
+    title: '1th Instructor',
+    description: 'Jurica is CEO of the marketing platform HubSpot, which he co-founded in 2006. ',
+    Image: 'img/jurica.jpg',
+  },
+  {
+    name: 'Sara Dam',
+    title: '2nd Instructor',
+    description: 'Fine artist-turned-coder, Sara Dam took a total immersion approach to learning web development.',
+    Image: 'img/sara.jpg',
+  },
+  {
+    name: 'Ayo Ogunseinde',
+    title: '3st Instructor',
+    description: 'Ayo the CEO of Smartsheet, a software-as-service (SaaS) company that offers applications for collaboration.',
+    Image: 'img/ayo.jpg',
+  },
+];
+
+instructorContainer.forEach((cardArtist) => {
+  const content = ` 
+    <div class="instructor">
+    <div>
+     <img class="image-instructor" src="${cardArtist.Image}" alt="Instructor-image">
+    </div>
+    <div class="cards">
+     <h2>${cardArtist.name}</h2>
+        <h3>${cardArtist.title}</h3>
+
+        <p class="demo-text">${cardArtist.description}</p>
+    </div>
+            </div>
+    `;
+  instructorSection.innerHTML += content;
+});
