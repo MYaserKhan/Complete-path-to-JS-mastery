f6192a2d65c690c7a5418cd6bbcf20f446d387e7
Yaser <khanyaser007@gmail.com>
2022-09-21 01:13:54 +0500

    Update index.html, style.css and main.js for UI/UX

--- js/main.js
+++ js/main.js
@@ -18,41 +18,40 @@ seeMore.addEventListener(('click'), () => {
 const instructorContainer = [
   {
     name: 'Joseph Gonzalez',
-    title: 'Main Instructor',
+    title: 'Leading open-source projects at GitHub',
     description: 'Joseph has been leading open-source projects at GitHub. Mostly related to JavaScript',
     Image: 'img/joseph.jpg',
   },
   {
     name: 'Jurica Koletic',
-    title: '1th Instructor',
+    title: 'CEO of the marketing platform HubSpot',
     description: 'Jurica is CEO of the marketing platform HubSpot, which he co-founded in 2006. ',
     Image: 'img/jurica.jpg',
   },
   {
     name: 'Sara Dam',
-    title: '2nd Instructor',
+    title: 'Fine artist-turned-coder',
     description: 'Fine artist-turned-coder, Sara Dam took a total immersion approach to learning web development.',
     Image: 'img/sara.jpg',
   },
   {
     name: 'Ayo Ogunseinde',
-    title: '3st Instructor',
+    title: 'CEO of Smartsheet',
     description: 'Ayo the CEO of Smartsheet, a software-as-service (SaaS) company that offers applications for collaboration.',
     Image: 'img/ayo.jpg',
   },
 ];
 
-instructorContainer.forEach((cardArtist) => {
+instructorContainer.forEach((cardInstructor) => {
   const content = ` 
     <div class="instructor">
     <div>
-     <img class="image-instructor" src="${cardArtist.Image}" alt="Instructor-image">
+     <img class="image-instructor" src="${cardInstructor.Image}" alt="Instructor-image">
     </div>
     <div class="cards">
-     <h2>${cardArtist.name}</h2>
-        <h3>${cardArtist.title}</h3>
-
-        <p class="demo-text">${cardArtist.description}</p>
+     <h2>${cardInstructor.name}</h2>
+        <h3>${cardInstructor.title}</h3>
+        <p class="demo-text">${cardInstructor.description}</p>
     </div>
             </div>
     `;
